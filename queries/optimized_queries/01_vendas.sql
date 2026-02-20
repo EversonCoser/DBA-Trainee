3ba95d8f3d26a191fdc7c0fb66e10c73855e9596
Everson Coser <cosereverson@gmail.com>
2026-02-20 10:17:47 -0300

    Consulta de faturamento mensal otimizada

--- queries/optimized_queries/01_vendas.sql
+++ queries/optimized_queries/01_vendas.sql
@@ -1,6 +1,5 @@
 -- Query para análise de vendas no ano de 2024, considerando apenas os pedidos com status 'Pago'.
 
-EXPLAIN ANALYZE
 SELECT 
     COUNT(*) AS total_vendas,
     SUM(valor_total) AS faturamento_total,
@@ -8,3 +7,17 @@ SELECT
 FROM vendas
     WHERE status_pedido = 'Pago' 
     AND data_venda BETWEEN '2024-01-01' AND '2024-12-31';
+
+-- Faturamento por mês, quantidade de vendas e ticket médio, considerando apenas os pedidos 
+-- com status 'Pago' no ano de 2024, ordenamento pelo faturamento.
+
+SELECT 
+    DATE_TRUNC('month', data_venda) AS mes,
+    SUM(valor_total) AS faturamento,
+    COUNT(*) AS total_vendas,
+    ROUND(AVG(valor_total), 2) AS ticket_medio
+FROM vendas
+    WHERE status_pedido = 'Pago'
+    AND data_venda BETWEEN '2024-01-01' AND '2024-12-31'
+    GROUP BY mes
+    ORDER BY faturamento DESC;
