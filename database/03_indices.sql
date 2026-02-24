0f0a3a61f978e8aa48e769ceb5b76fd7b0c23121
Everson Coser <cosereverson@gmail.com>
2026-02-24 10:19:29 -0300

    Índices para otimizar a consulta de performance por funcionários

--- database/03_indices.sql
+++ database/03_indices.sql
@@ -1,3 +1,5 @@
+-- vendas
+
 CREATE INDEX idx_vendas_data_venda 
 ON vendas (data_venda);
 
@@ -7,3 +9,14 @@ ON vendas (status_pedido, data_venda);
 CREATE INDEX idx_vendas_covering
 ON vendas (status_pedido, data_venda)
 INCLUDE (valor_total);
+
+CREATE INDEX idx_vendas_pago_cover
+ON vendas (data_venda, id_funcionario)
+INCLUDE (valor_total)
+WHERE status_pedido = 'Pago';
+
+-- pessoas
+
+CREATE INDEX idx_pessoas_ativas
+ON pessoas (id_pessoa)
+WHERE ativo = true;
